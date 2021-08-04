af97292772adc982497cb6a584b1440563566149
Sreemannarayana Ikkurthi <ikkurthis1998@gmail.com>
2021-08-04 20:12:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# mock-stock-api
+ API for mark14 , Level 0 of neoG Camp
---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+let value = 110;
+
+setInterval(() => {
+	if (Math.floor(Math.random() * 10) % 2 === 0) {
+		value = value + Math.random() * 100;
+	} else {
+		value = value - Math.random() * 100;
+	}
+	if (value < 0) {
+		value = -value;
+	}
+}, 24 * 60 * 60 * 1000);
+
+app.get("/getValue", (req, res) => {
+	res.send(`${value}`);
+});
+
+app.listen(process.env.PORT || port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
